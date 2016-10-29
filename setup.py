e191290905cab73ff7636a416ebbff556a7fd52c
Marcin Paszkiewicz <marcin@Marcins-MacBook-Air.local>
2016-10-29 22:59:55 +0200

    Remove bin directory

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='vse',
-      version='0.1.1',
+      version='0.1.2',
       author='Marcin K. Paszkiewicz',
       author_email='mkpaszkiewicz@gmail.com',
       description='Configurable visual search engine based on the OpenCV',
       url='https://github.com/mkpaszkiewicz/vse',
-      download_url='https://github.com/mkpaszkiewicz/vse/tarball/0.1.1',
-      packages=['vse', 'bin'],
+      download_url='https://github.com/mkpaszkiewicz/vse/tarball/0.1.2',
+      packages=['vse'],
       keywords=['visual', 'search', 'engine', 'computer', 'vision'],
       install_requires=[
           'NumPy',
